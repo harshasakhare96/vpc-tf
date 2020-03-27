2d9de70e689fe19d9b38e5c5de6dabba31b1f47d
root <root@webwerks-Latitude-E7240>
2020-03-27 12:51:33 +0530

    this is a main script

---
+++ main.tf
@@ -0,0 +1,188 @@
+data "aws_availability_zones" "available" {
+state = "available"
+}
+ 
+resource "aws_vpc" "tf_vpc" {
+cidr_block = "${var.vpc_cidr}"
+enable_dns_hostnames = true
+enable_dns_support   = true
+tags = {
+    Name = "tf_vpc"
+  }
+}
+
+
+resource "aws_internet_gateway" "tf_gw" {
+vpc_id = "${aws_vpc.tf_vpc.id}"
+tags = {
+Name = "tf_igw"
+ }
+
+}
+
+
+
+resource "aws_route_table" "tf_public_rt" {
+vpc_id = "${aws_vpc.tf_vpc.id}"
+route {
+cidr_block = "0.0.0.0/0"
+gateway_id = "${aws_internet_gateway.tf_gw.id}"
+}
+tags =  {
+Name = "tf_public_rt"
+}
+
+}
+
+
+
+resource "aws_eip" "tf_eip" {
+vpc =true
+
+}
+
+
+
+
+resource "aws_subnet" "tf_public_subnet_1" {
+    vpc_id = "${ aws_vpc.tf_vpc.id }"
+    cidr_block = "${ var.public_cidr_1}"
+    map_public_ip_on_launch = true 
+    availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags = {
+
+  Name = "tf_public_1"
+
+   }
+
+}
+
+
+
+resource "aws_subnet" "tf_public_subnet_2" {
+vpc_id = "${ aws_vpc.tf_vpc.id }"
+cidr_block = "${ var.public_cidr_2}"
+map_public_ip_on_launch = true
+availability_zone = "${data.aws_availability_zones.available.names[1]}"
+tags = {
+Name = "tf_public_2"
+
+ }
+
+}
+
+
+
+resource "aws_subnet" "tf_private_subnet_1" {
+vpc_id = "${ aws_vpc.tf_vpc.id }"
+cidr_block = "${ var.private_cidr_1}"
+map_public_ip_on_launch = false
+availability_zone = "${data.aws_availability_zones.available.names[0]}"
+tags = {
+Name = "tf_private_1"
+
+}
+
+}
+
+
+
+resource "aws_subnet" "tf_private_subnet_2" {
+vpc_id = "${ aws_vpc.tf_vpc.id }"
+cidr_block = "${ var.private_cidr_2}"
+map_public_ip_on_launch = false
+availability_zone = "${data.aws_availability_zones.available.names[1]}"
+tags = {
+Name = "tf_private_2"
+}
+
+}
+
+
+
+resource "aws_route_table_association" "tf_public_assoc_1" {
+subnet_id = "${ aws_subnet.tf_public_subnet_1.id}"
+route_table_id = "${ aws_route_table.tf_public_rt.id}"
+
+}
+
+
+
+
+resource "aws_route_table_association" "tf_public_assoc_2" {
+subnet_id = "${ aws_subnet.tf_public_subnet_2.id}"
+route_table_id = "${ aws_route_table.tf_public_rt.id}"
+
+}
+
+
+
+resource "aws_nat_gateway" "tf_nat_gateway" {
+allocation_id = "${ aws_eip.tf_eip.id }"
+subnet_id = "${ aws_subnet.tf_public_subnet_1.id }"
+tags = {
+Name = "gw NAT"
+
+}
+
+}
+
+
+resource "aws_default_route_table" "tf_private_rt" {
+default_route_table_id = "${aws_vpc.tf_vpc.default_route_table_id}"
+
+route {
+cidr_block = "0.0.0.0/0"
+nat_gateway_id = "${ aws_nat_gateway.tf_nat_gateway.id}"
+}
+tags = {
+Name = "tf_private_rt"
+}
+
+}
+
+
+resource "aws_route_table_association" "tf_private_assoc_1" {
+subnet_id = "${ aws_subnet.tf_private_subnet_1.id }"
+route_table_id = "${ aws_default_route_table.tf_private_rt.id }"
+}
+
+
+
+resource "aws_route_table_association" "tf_private_assoc_2" {
+subnet_id = "${ aws_subnet.tf_private_subnet_2.id }"
+route_table_id = "${ aws_default_route_table.tf_private_rt.id }"
+}
+
+
+
+resource "aws_security_group" "tf_public_sg" {
+name = "tf_public_sg"
+description = " used for access for the public instance"
+vpc_id = "${aws_vpc.tf_vpc.id}"
+
+ingress {
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["${var.accessip}"]
+}
+
+ingress {
+from_port = 80
+to_port = 80
+protocol = "tcp"
+cidr_blocks = ["${var.accessip}"]
+
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks =  ["0.0.0.0/0"]
+}
+}
+
+
